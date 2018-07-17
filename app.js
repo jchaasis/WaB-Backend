433a6e973f0ef6c9c64abbcb2b17934fc0e9fd4f
jchaasis <jchaasis@gmail.com>
2018-07-17 17:48:09 -0400

    Backend responds to get request.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyparser = require('body-parser');
 
 
 //establish server
+const port = process.env.PORT || 4000;
 const app = express();
 //initialize bodyparser
 app.use(bodyparser.urlencoded({ extended: false }));
@@ -14,7 +15,35 @@ const db = new Sequelize('whackaBrady', 'christianhaasis', '', {
     dialect: 'postgres',
 });
 
+const Score = db.define('score', {
+    player: Sequelize.STRING,
+    score: Sequelize.INTEGER,
+});
 
-app.listen(3003, function(){
-    console.log("Whack awhay on on port 3003");
+// Sychronize the 'todo' schema with the database, meaning make
+// sure all tables exist and have the right fields.
+Score.sync().then(function () {
+    console.log('scores has synced')
+    // Score.create({
+    //      player: 'TST',
+    //      score: 7,
+    // });
+});
+
+//establish routes
+
+  //get routes
+  //get all the scores to display on the sidebar
+  app.get("/", function(req, res){
+    console.log(req);
+    console.log('received the get request');
+    Score.findAll().then((items)=>{
+      res.send({
+                Scores: items,
+      });
+    });
+  });
+
+app.listen(port, function(){
+    console.log("Whack awhay on on port 4000");
   });
